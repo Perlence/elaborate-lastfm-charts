942504d07fb6950b6e329727d36f684ce5fa57d4
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2014-11-11 00:10:22 +0300

    Bump version to '0.1.2'

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='elaborate-lastfm-charts',
     description='Amazing Last.fm charts.',
-    version='0.1.1',
+    version='0.1.2',
     author='Sviatoslav Abakumov, Anastasiya Kozayeva',
     author_email='dust.harvesting@gmail.com',
     url='https://github.com/Perlence/elaborate-lastfm-charts',
